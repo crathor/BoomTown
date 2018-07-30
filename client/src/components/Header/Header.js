13a54ec5503162e916103790816df856db0b3182
Cody Rathor <codyrathor@gmail.com>
2018-07-29 22:45:24 -0700

    fixed issue with shareitem link not coming back when hitting backbutton in browser

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import {
   withStyles,
@@ -12,64 +12,53 @@ import AddIcon from '@material-ui/icons/AddCircle'
 import { Link } from 'react-router-dom'
 import Menu from './Menu'
 import BoomTownLogo from '../../images/boomtown.svg'
-
 import styles from './styles'
 import { ViewerContext } from '../../context/ViewerProvider'
+import { withRouter } from 'react-router-dom'
 
-class Header extends Component {
-  state = {
-    sharePageActive: false
-  }
-  hideShareButton = () => {
-    this.setState({ sharePageActive: true })
-  }
-  showShareButton = () => {
-    this.setState({ sharePageActive: false })
-  }
-  render() {
-    const { classes } = this.props
-    return (
-      <AppBar position="sticky" className={classes.root}>
-        <Toolbar disableGutters className={classes.toolbar}>
-          <IconButton
-            className={classes.homeButton}
-            color="inherit"
-            aria-label="Menu"
-            to="/items"
+const Header = ({ classes, location }) => {
+  return (
+    <AppBar position="sticky" className={classes.root}>
+      <Toolbar disableGutters className={classes.toolbar}>
+        <IconButton
+          className={classes.homeButton}
+          color="inherit"
+          aria-label="Menu"
+          to="/items"
+          component={Link}
+          onClick={this.showShareButton}
+        >
+          <img src={BoomTownLogo} alt="BoomTown" className={classes.logo} />
+        </IconButton>
+        <Slide in={location.pathname !== '/share'} direction="left">
+          <Button
+            onClick={this.hideShareButton}
             component={Link}
-            onClick={this.showShareButton}
+            to="/share"
+            variant="extendedFab"
+            color="primary"
+            className={classes.button}
           >
-            <img src={BoomTownLogo} alt="BoomTown" className={classes.logo} />
-          </IconButton>
-          <Slide in={!this.state.sharePageActive} direction="left">
-            <Button
-              onClick={this.hideShareButton}
-              component={Link}
-              to="/share"
-              variant="extendedFab"
-              color="primary"
-              className={classes.button}
-            >
-              <AddIcon style={{ marginRight: 8 }} />
-              Share Something
-            </Button>
-          </Slide>
-          <ViewerContext.Consumer>
-            {({ viewer }) => (
-              <Menu
-                showShareButton={this.showShareButton}
-                currentViewer={viewer.id}
-              />
-            )}
-          </ViewerContext.Consumer>
-        </Toolbar>
-      </AppBar>
-    )
-  }
+            <AddIcon style={{ marginRight: 8 }} />
+            Share Something
+          </Button>
+        </Slide>
+        <ViewerContext.Consumer>
+          {({ viewer }) => (
+            <Menu
+              showShareButton={this.showShareButton}
+              currentViewer={viewer.id}
+            />
+          )}
+        </ViewerContext.Consumer>
+      </Toolbar>
+    </AppBar>
+  )
 }
 
 Header.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired
 }
 
-export default withStyles(styles)(Header)
+export default withRouter(withStyles(styles)(Header))
